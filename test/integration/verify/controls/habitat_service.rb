da3b5390a884c9caedccad12dcb17425bbe0f619
David McCown <dmccown@chef.io>
2018-11-05 11:29:50 -0600

    Adds integration test using train plugin
    Signed-off-by: David McCown <dmccown@chef.io>

---
+++ test/integration/verify/controls/habitat_service.rb
@@ -0,0 +1,11 @@
+control 'habitat_service' do
+  describe habitat_service(origin: 'core', name: 'nginx') do
+    it                     { should exist }
+    its('version')         { should cmp >= '1.15.5' }
+    its('topology')        { should eq 'standalone' }
+    its('update_strategy') { should eq 'none' }
+    its('deps.size')       { should eq 10 }
+    its('pkg_id')          { should match /core\/nginx\/\d+.\d+\.\d+\/\d{14}$/ }
+    its('release')         { should cmp >= 20180608050617 }
+  end
+end
