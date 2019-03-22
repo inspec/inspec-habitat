31969269b45c878e8d85a95436aaa9feebf936aa
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-03-21 20:58:26 -0400

    A really ugly fixture loader
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/unit_helper.rb
+++ test/unit/unit_helper.rb
@@ -7,6 +7,10 @@ require 'train-habitat'
 require 'byebug'
 require 'json'
 
+class Module
+  include Minitest::Spec::DSL
+end
+
 #============================================================================#
 #                          Actual Test Helper
 #============================================================================#
@@ -28,17 +32,73 @@ module InspecHabitat
       end
     end
 
-    def seek_test(_opts)
-      # TODO
+    def seek_test(opts)
       describe 'when seeking the resource' do
+        let(:unit_test_path) { File.expand_path(File.join('test', 'unit')) }
+        let(:unit_fixture_path) { File.join(unit_test_path, 'fixtures') }
+
         describe 'when the resource is locatable' do
-          it 'should be found'
+          opts[:hit_params].each do |hit_param_set|
+            it "should be found with #{hit_param_set}" do
+              InspecHabitat::UnitTestHelper.mock_inspec_context_object(self, opts[:fixture])
+              obj = opts[:klass].new(hit_param_set) # No exception
+              obj.wont_be_nil
+              obj.exists?.must_equal true
+            end
+          end
         end
-        describe 'when the resource is not locatable' do
-          it 'should throw an exception'
-        end
+
+        # describe 'when the resource is not locatable' do
+        #   it 'should throw an exception'
+        # end
       end
     end
+
+    # How to setup fixtures:
+    # CLI:
+    # API:
+    # DRYing this up was very difficult, and I am sure there is a better way.
+    def mock_inspec_context_object(test_cxt, fixture)
+      test_cxt.instance_eval do
+        inspec_cxt = mock
+        hab_cxn = mock
+
+        inspec_cxt.stubs(:backend).returns(hab_cxn)
+
+        if fixture.key?(:cli)
+          hab_cxn.stubs(:cli_options_provided?).returns(true)
+          run_result = mock
+          run_result.stubs(:exit_status).returns(fixture[:cli][:exit_status])
+
+          out = fixture[:cli][:stdout_file] ? File.read(File.join(unit_fixture_path, fixture[:cli][:stdout_file])) : ''
+          run_result.stubs(:stdout).returns(out)
+          err = fixture[:cli][:stderr_file] ? File.read(File.join(unit_fixture_path, fixture[:cli][:stderr_file])) : ''
+          run_result.stubs(:stderr).returns(err)
+
+          hab_cxn.stubs(:run_hab_cli).with(fixture[:cli][:cmd]).returns(run_result)
+        else
+          hab_cxn.stubs(:cli_options_provided?).returns(false)
+        end
+
+        if fixture.key?(:api)
+          hab_cxn.stubs(:api_options_provided?).returns(true)
+          htg = mock
+          hab_cxn.stubs(:habitat_api_client).returns(htg)
+          resp = mock
+          resp.stubs(:code).returns(fixture[:api][:code])
+          if fixture[:api][:file]
+            api_path = File.join(unit_fixture_path, fixture[:api][:file])
+            resp.stubs(:body).returns(JSON.parse(File.read(api_path), symbolize_names: true))
+          end
+          htg.stubs(:get_path).with(fixture[:api][:path]).returns(resp)
+        else
+          hab_cxn.stubs(:api_options_provided?).returns(false)
+        end
+        Inspec::Plugins::Resource.any_instance.stubs(:inspec).returns(inspec_cxt)
+
+      end
+    end
+    module_function :mock_inspec_context_object
   end
 end
 
@@ -74,6 +134,12 @@ module Inspec
       def self.example(val)
         @metadata ||= OpenStruct.new
         @metadata[:example] = val
+      end
+
+      # Mock inspec context object
+      # Will be re-mocked in various places
+      def inspec
+        nil
       end
     end
   end
