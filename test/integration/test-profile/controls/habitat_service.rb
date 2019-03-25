0efbd3e8803989e023f18a1847df6144b6e9a352
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-03-25 15:44:06 -0400

    Update to make certain properties into matchers
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/test-profile/controls/habitat_service.rb
+++ test/integration/test-profile/controls/habitat_service.rb
@@ -2,10 +2,10 @@ control 'habitat_service' do
   describe habitat_service(origin: 'core', name: 'httpd') do
     it                     { should exist }
     its('version')         { should cmp >= '2.3.45' }
-    its('topology')        { should eq 'standalone' }
-    its('update_strategy') { should eq 'none' }
-    its('dependencies.size')       { should eq 28 }
+    its('dependency_names.size')       { should eq 28 }
     its('pkg_id')          { should match %r(core\/httpd\/\d+.\d+\.\d+\/\d{14}$) }
     its('release')         { should cmp >= '20180608050617' }
+    it { should have_standalone_topology }
+    it { should be_updated_by_none }
   end
 end
