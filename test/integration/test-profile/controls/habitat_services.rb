093f46a13ee28988042eb347350b9aa707a185f1
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-03-27 17:54:47 -0400

    Linting
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/test-profile/controls/habitat_services.rb
+++ test/integration/test-profile/controls/habitat_services.rb
@@ -1,21 +1,21 @@
 control 'habitat_services' do
   # Properties check
   describe habitat_services do
-    it                     { should exist }
+    it { should exist }
     its('count') { should eq 2 }
-    its('topologies')      { should eq ['standalone'] }
+    its('topologies') { should eq ['standalone'] }
     its('update_strategies') { should eq ['none'] }
     its('dependency_names') { should include 'core/pcre' }
     its('dependency_names') { should_not include 'core/gcc' }
-    its('names') { should include 'httpd'}
-    its('names') { should include 'memcached'}
+    its('names') { should include 'httpd' }
+    its('names') { should include 'memcached' }
   end
 
   # Filtering
   describe habitat_services.where(name: 'httpd') do
     its('count') { should cmp 1 }
   end
-  describe habitat_services.where { release >= '20181231000000'} do
+  describe habitat_services.where { release >= '20181231000000' } do # rubocop:disable Lint/AmbiguousBlockAssociation
     its('count') { should cmp 2 }
   end
 end
