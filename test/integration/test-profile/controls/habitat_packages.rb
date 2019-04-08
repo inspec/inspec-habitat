73c711cd86989e9fbd4286fb5469cccf3a15a447
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-04-08 11:28:40 -0400

    Passing integration tests
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

---
+++ test/integration/test-profile/controls/habitat_packages.rb
@@ -0,0 +1,17 @@
+control 'habitat_packages' do
+  # Properties check
+  describe habitat_packages do
+    it { should exist }
+    its('count') { should be >= 37 }
+    its('names') { should include 'httpd' }
+    its('names') { should include 'memcached' }
+  end
+
+  # Filtering
+  describe habitat_packages.where(name: 'httpd') do
+    its('count') { should cmp 1 }
+  end
+  describe habitat_packages.where { release >= '20181231000000' } do # rubocop:disable Lint/AmbiguousBlockAssociation
+    its('count') { should >= 2 }
+  end
+end
